d2562545650b4bcd4393f0de41903877dcf7c493
dulanjalidilmi <dulanjalidilmi@gmail.com>
2020-12-07 11:17:35 +0530

    Add ui templates

---
+++ monitoring-dashboard/components/org.wso2.micro.integrator.dashboard.web/web-app/src/commons/SideDrawer.js
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ *
+ */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { Button, Table, TableCell, TableRow } from '@material-ui/core';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import Switch from "react-switch";
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import XMLViewer from 'react-xml-viewer';
+import Box from '@material-ui/core/Box';
+
+export default function SideDrawer(props) {
+    var artifactName = props.proxyName;
+    var nodeData = props.nodeData;
+
+    const [open, setOpen] = React.useState(false);
+
+    const openSourceViewPopup = () => {
+        setOpen(true);
+    }
+
+    const closeSourceViewPopup = () => {
+        setOpen(false);
+    };
+
+    const descriptionElementRef = React.useRef(null);
+    React.useEffect(() => {
+        if (open) {
+            const { current: descriptionElement } = descriptionElementRef;
+            if (descriptionElement !== null) {
+                descriptionElement.focus();
+            }
+        }
+    }, [open]);
+
+    const classes = useStyles();
+
+    const changeServiceStatus = () => {
+        nodeData['isActive'] = !nodeData['isActive'];
+    };
+
+    const changeTracingStatus = () => {
+        nodeData.details['tracing'] = !nodeData.details['tracing'];
+    };
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper className={classes.sideDrawerHeading} square>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            {artifactName}
+                        </Typography>
+                        <Typography variant="h8" color="inherit" noWrap>
+                            {nodeData['nodeId']}
+                        </Typography>
+                    </Paper>
+                    <Paper className={classes.paper}>
+                        <Table>
+                            <TableRow>
+                                <TableCell>Service Status</TableCell>
+                                <TableCell><Switch checked={nodeData['isActive']} onChange={changeServiceStatus} /></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Service Name</TableCell>
+                                <TableCell>{artifactName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Statistics</TableCell>
+                                <TableCell>{nodeData.details['statistics']}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Tracing</TableCell>
+                                <TableCell>
+                                    <label>
+                                        <Switch checked={nodeData.details['tracing']} onChange={changeTracingStatus}/>
+                                    </label>
+                                </TableCell>
+                            </TableRow>
+                        </Table>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper} square>
+                        <Typography variant="h6" color="inherit" noWrap className={classes.subTopic}>
+                            Endpoints
+                        </Typography>
+                        <hr className={classes.horizontalLine}></hr>
+                    </Paper>
+                    <Paper className={classes.paper} square>
+                        <Table>
+                            {nodeData.endpoints.map(ep =>
+                                <TableRow>{ep}
+                                    <CopyToClipboard text={ep} className={classes.clipboard}>
+                                        <Button><FileCopyIcon/></Button>
+                                    </CopyToClipboard>
+                                </TableRow>)}
+                        </Table>
+                    </Paper><br/>
+                    <Box textAlign='center'>
+                        <Button onClick={() => openSourceViewPopup()} variant="contained" color="primary">Source View</Button>
+                        <Dialog
+                            open={open}
+                            onClose={closeSourceViewPopup}
+                            aria-labelledby="scroll-dialog-title"
+                            aria-describedby="scroll-dialog-description"
+                            classes={classes.sourceView}
+                        >
+                            <DialogTitle id="scroll-dialog-title">{artifactName}</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText
+                                    id="scroll-dialog-description"
+                                    ref={descriptionElementRef}
+                                    tabIndex={-1}>
+                                    <XMLViewer xml={nodeData.source} />
+                                </DialogContentText>
+                            </DialogContent>
+                        </Dialog>
+                    </Box>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        color: theme.palette.text.secondary,
+    },
+    sideDrawerHeading: {
+        padding: theme.spacing(1),
+        height: '64px',
+        backgroundColor: '#3f51b5',
+        color: '#ffffff'
+    },
+    subTopic: {
+        color: '#3f51b5'
+    },
+    horizontalLine : {
+        backgroundColor : '#3f51b5',
+        borderWidth: '0px',
+        height: '1px'
+    },
+    clipboard: {
+        color: '#3f51b5'
+    },
+    sourceView: {
+        backgroundColor: 'red',
+        width: '2000px',
+    }
+}));
---
+++ monitoring-dashboard/components/org.wso2.micro.integrator.dashboard.web/web-app/src/home/NodeFilter.js
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ *
+ */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        maxWidth: 300,
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    chip: {
+        margin: 2,
+    },
+    noLabel: {
+        marginTop: theme.spacing(3),
+    },
+}));
+
+export default class NodeFilter extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {nodeList : ['node_01', 'node_02', 'node_03', 'node_04']};
+    }
+
+    render() {
+        return <MultipleSelect nodeList={this.state.nodeList}/>
+    }
+}
+
+function MultipleSelect(props) {
+    var NodeList = props.nodeList;
+    const classes = useStyles();
+    const [nodeId, setNodeId] = React.useState([]);
+
+    const handleChange = (event) => {
+        setNodeId(event.target.value);
+    };
+
+    return (
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Select Nodes</InputLabel>
+                <Select
+                    multiple
+                    value={nodeId}
+                    onChange={handleChange}
+                    renderValue={(selected) => (
+                        <div className={classes.chips}>
+                            {selected.map((value) => (
+                                <Chip key={value} label={value} className={classes.chip} />
+                            ))}
+                        </div>
+                    )}
+                >
+                    {NodeList.map((name) => (
+                        <MenuItem key={name} value={name}>
+                            <Checkbox checked={nodeId.indexOf(name) > -1} />
+                            <ListItemText primary={name} />
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
+    );
+}
