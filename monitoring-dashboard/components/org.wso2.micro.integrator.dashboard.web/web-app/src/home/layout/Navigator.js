f7c8b1a82ecadba57e2b429a5b0f40701e408d22
Rosen Silva <rosen@live.com>
2023-03-05 12:57:59 +0530

    Update Navigator.js
    for 4.2.0 rc release

--- monitoring-dashboard/components/org.wso2.micro.integrator.dashboard.web/web-app/src/home/layout/Navigator.js
+++ monitoring-dashboard/components/org.wso2.micro.integrator.dashboard.web/web-app/src/home/layout/Navigator.js
@@ -106,7 +106,7 @@ function Navigator(props) {
                 </ListItem>
                 <ListItem className={classes.versionLabel}>
                     <ListItemText>
-                        Version : 4.2.0-beta
+                        Version : 4.2.0
                     </ListItemText>
                 </ListItem>
                 <ListItem 
